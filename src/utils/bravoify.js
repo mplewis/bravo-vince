cc3133630a7ed8a399a3873e825b81f329fbfdb3
Matt Lewis <matt@mplewis.com>
2017-04-25 22:46:10 -0500

    dont let a word be used twice

--- src/utils/bravoify.js
+++ src/utils/bravoify.js
@@ -3,10 +3,13 @@ export default function (input) {
   const vert = words[0].split('')
   const horiz = words.slice(1)
   horiz.forEach(h => {
+    let found = false
     vert.forEach((v, i) => {
+      if (found) return
       if (v.length > 1) return
       if (v !== h[0]) return
       vert[i] = h.split('').join(' ')
+      found = true
     })
   })
   return vert.join('\n')
